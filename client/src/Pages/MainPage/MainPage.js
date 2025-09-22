191444c1001f58fad772ace7d4f2c6d6aeeaf065
jdy031017js <149340377+jdy031017js@users.noreply.github.com>
2025-09-22 17:18:35 +0900

    Button moving

--- client/src/Pages/MainPage/MainPage.js
+++ client/src/Pages/MainPage/MainPage.js
@@ -1,9 +1,18 @@
-import { Container } from "./MainPage.style.js";
-import CustomButton from "../../Component/CustomButton/CustomButton.js";
-import Header from "../../Component/Header/Header.js";
+import { Container } from './MainPage.style.js';
+import CustomButton from '../../Component/CustomButton/CustomButton.js';
+import Header from '../../Component/Header/Header.js';
+import { useNavigate } from 'react-router-dom';
 function MainPage() {
-    return <Container><Header/><CustomButton ButtonMessage="시작하기 >" />  </Container>;
-
+    const navigate = useNavigate();
+    const handlechatButton = () => {
+        navigate('/chat');
+    };
+    return (
+        <Container>
+            <Header />
+            <CustomButton ButtonMessage="시작하기 >" ButtonOnClick={handlechatButton} />
+        </Container>
+    );
 }
 
 export default MainPage;

1fc061015591e9f356d50fdb6e5410df8f90e235
jdy031017js <149340377+jdy031017js@users.noreply.github.com>
2025-09-22 17:00:17 +0900

    Header create #15
    처음으로 만들어본 헤더
    9.22 소정언니랑 함께 ^^

--- client/src/Pages/MainPage/MainPage.js
+++ client/src/Pages/MainPage/MainPage.js
@@ -1,7 +1,9 @@
 import { Container } from "./MainPage.style.js";
-// import CustomButton from "../../Component/CustomButton/CustomButton.js";
+import CustomButton from "../../Component/CustomButton/CustomButton.js";
+import Header from "../../Component/Header/Header.js";
 function MainPage() {
-    return <Container>{/* <CustomButton ButtonMessage="시작하기 >" /> */}</Container>;
+    return <Container><Header/><CustomButton ButtonMessage="시작하기 >" />  </Container>;
+
 }
 
 export default MainPage;

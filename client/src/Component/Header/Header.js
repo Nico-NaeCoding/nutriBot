803e79721284d58325d4e212312b590d691e7c95
jdy031017js <149340377+jdy031017js@users.noreply.github.com>
2025-09-29 23:48:02 +0900

    feat: add framer-motion and update Header link
    - Installed framer-motion for page transition animations
    - Updated Header to make logo clickable and navigate home

--- client/src/Component/Header/Header.js
+++ client/src/Component/Header/Header.js
@@ -1,9 +1,21 @@
 import { Container, Headericone, Headertitle } from './Header.style.js';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LogoLink = styled(Link)`
+    display: flex;
+    align-items: center; /* 로고랑 텍스트를 세로 중앙 정렬 */
+    text-decoration: none;
+    color: inherit; /* 텍스트 색상 안 바뀌게 */
+`;
+
 const Header = () => {
     return (
         <Container>
-            <Headericone src="./images/green.png" />
-            <Headertitle>NutriBot</Headertitle>
+            <LogoLink to="/">
+                <Headericone src="./images/green.png" alt="logo" />
+                <Headertitle>NutriBot</Headertitle>
+            </LogoLink>
         </Container>
     );
 };
